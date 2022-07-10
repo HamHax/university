310e665a80802ee6f10dafd25537ae62fd8f1a3d
HamHax <hamo.haxverdyan2000@gmail.com>
2022-07-10 14:08:00 +0400

    react app starting process

---
+++ univers/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css"
+import React from "react"
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props)
+  }
+
+  render () {
+    return (
+      <section>
+
+      </section>
+    )
+  }
+}
+
+export default App
